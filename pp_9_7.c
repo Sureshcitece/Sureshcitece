510b2f6702aa5e7cb905012c2ff56bf5355946ea
sureshcitece <sureshcitece@gmail.com>
2026-03-19 15:36:32 +0530

    pp_9_7

---
+++ pp_9_7.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int square (int x){
+    return x * x;
+}
+
+int power(int x, int n){
+    if (n == 1) return x; 
+    if(n % 2 == 0) return square(power(x , n/2));
+    else return x * power(x, n-1);
+}
+
+int main(){
+    int x, n;
+    printf("Enter numbers to calculate power:");
+    scanf("%d %d", &x, &n);
+    printf("Power is %d", power(x, n));
+    return 0;
+}
